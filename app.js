5ad23b89bf74513b0641781c5ef20b0e35351197
renato-torres-93 <renatotbreis@gmail.com>
2022-05-19 15:28:31 -0300

    added more info to work with

--- app.js
+++ app.js
@@ -9,15 +9,27 @@ app.get ('/products', (req, res) =>
   const products = [
     {
       id: 1,
-      name: 'hammer'
+      name: 'guitar',
+      price: 480,
+      stock: 2
     },
     {
       id: 2,
-      name: 'screwdriver'
+      name: 'microphone',
+      price: 159.90,
+      stock: 5
     },
     {
       id: 3,
-      name: 'wrench'
+      name: 'amp head',
+      price: 200,
+      stock: 1
+    },
+    {
+      id: 4,
+      name: 'amp cabinet 4x12',
+      price: 629.99,
+      stock: 1
     }
   ]
 
