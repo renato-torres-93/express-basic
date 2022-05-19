2711ae01a6e0a0cd516f50a3923dc9fe46f301df
renato-torres-93 <renatotbreis@gmail.com>
2022-05-18 21:10:10 -0300

    porting local project to github

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const app = express ()
+const port = 3000
+
+app.get ('/', (req, res) => res.send ('I\'m not a teapot.'))
+
+app.get ('/products', (req, res) =>
+{
+  const products = [
+    {
+      id: 1,
+      name: 'hammer'
+    },
+    {
+      id: 2,
+      name: 'screwdriver'
+    },
+    {
+      id: 3,
+      name: 'wrench'
+    }
+  ]
+
+  res.json (products)
+})
+
+app.listen (port, () => console.log (`Listening on port ${port}...`))
